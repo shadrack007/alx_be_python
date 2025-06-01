d4ba77a9d95ffb356247c66986b411de1e07717b
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-06-01 10:42:59 +0300

    remove lower case character check

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -4,19 +4,19 @@ time_bound = input("Is it time-bound? (yes/no): ")
 
 match priority:
     case "high":
-        if time_bound.lower() == "yes":
+        if time_bound == "yes":
             print(f"Reminder: '{task}' is high priority task. Please complete it immediately.")
         else:
             print(f"Note: '{task}' is high priority task. Please complete it as soon as possible.")
        
     case "medium":
-        if time_bound.lower() == "yes":
+        if time_bound == "yes":
             print(f"Reminder: '{task}' is medium priority task. Please complete it within the next few days.")
         else:
             print(f"Note: '{task}' is medium priority task. Please complete it within the week.")
     
     case "low":
-        if time_bound.lower() == "yes":
+        if time_bound == "yes":
             print(f"Reminder: '{task}' is low priority task. Please complete it when you have time.")
         else:
             print(f"Note: '{task}' is low priority task. You can complete it at your convenience.")
