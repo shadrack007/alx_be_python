383b523b40380d7a049452a477eb05386155b052
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-06-01 10:58:03 +0300

    modified print message

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -5,6 +5,6 @@ if weather == 'sunny':
 elif weather == 'rainy':
     print('Don\'t forget your umbrella and raincoat')
 elif weather == 'cold':
-    print('Make sure to wear a warm jacket and a hat.')
+    print('Make sure to wear a warm coat and a scarf.')
 else:
     print('Sorry, I don\'t have recommendations for this weather.')
