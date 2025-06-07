011e6c91b127e4dc3b10240ea1d7b22eb6877092
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-06-07 21:30:41 +0300

    fix: renamed parameters

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,15 @@
-def perform_operation(numb1, numb2, operation):
+def perform_operation(num1, num2, operation):
     match operation:
         case 'add':
-            result = numb1 + numb2
+            result = num1 + num2
         case 'subtract':
-            result = numb1 - numb2
+            result = num1 - num2
         case 'multiply':
-            result = numb1 * numb2
+            result = num1 * num2
         case 'divide':
-            if numb2 == 0:
+            if num2 == 0:
                 return "You can not divide by zero"
-            result = numb1 / numb2
+            result = num1 / num2
         case _:
             return 'Unknown operation specified'
     return result
